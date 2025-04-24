daf878de4d31cbdbde39af33f1d79b03d8f6d287
kl755999 <kl755999@LS302D24>
2025-04-24 18:45:32 -0500

    CreatedAt for Order automatically gets the current DateTime when creating Order object

--- Models/Order.cs
+++ Models/Order.cs
@@ -5,19 +5,18 @@ namespace CurrencyExchange.Models
 {
     public class Order
     {
-        int OrderID { get; set; }
-        int UserID { get; set; }
-        String? Type { get; set; }
+        public int OrderID { get; set; }
+        public int UserID { get; set; }
+        public String? Type { get; set; }
 
-        [Required(ErrorMessage = "Enter a price")]
         [Column(TypeName = "decimal(8,2)")]
         [DisplayFormat(DataFormatString = "{0:C}")]
-        Decimal? Price { get; set; }
+        public Decimal? Price { get; set; }
 
         [Required(ErrorMessage = "Enter a quantity")]
-        int Quantity { get; set; }
-        int Remaining { get; set; }
-        String? Status { get; set; }
-        DateTime CreatedAt => new DateTime()
+        public int Quantity { get; set; }
+        public int Remaining { get; set; }
+        public String? Status { get; set; }
+        public DateTime CreatedAt { get; set; } = DateTime.Now;
     }
 }
