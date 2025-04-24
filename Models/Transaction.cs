f4d856f4afac992e44fec9316dd1342863212175
Leiracal <leiramontgomery@gmail.com>
2025-04-24 18:26:55 -0500

    Created User and Transactio models.

---
+++ Models/Transaction.cs
@@ -0,0 +1,22 @@
+﻿namespace CurrencyExchange.Models
+{
+    public class Transaction
+    {
+        private int TransactionID { get; set; }
+
+        // OrderID for buy order
+        private int BuyOrderID { get; set; }
+
+        // OrderID for sell order
+        private int SellOrderID { get; set; }
+
+        // Quantity of VC exchanged
+        private int Quantity { get; set; }
+
+        // Price per 1 VC
+        private decimal Price { get; set; }
+
+        // Date/time that transaction was completed
+        private DateTime FulfilledAt { get; set; }
+    }
+}
---
+++ Models/User.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Identity;
+using System.ComponentModel.DataAnnotations;
+
+namespace CurrencyExchange.Models
+{
+    public class User : IdentityUser
+    {
+
+        // Active, unlocked Real Money balance
+        private decimal RMTBalance { get; set; }
+
+        // RMT balance locked in buy limit orders
+        private decimal RMTLocked { get; set; }
+
+        // Active, unlocked VC balance
+        private int VCBalance { get; set; }
+        
+        // VC balance locked in sell limit orders
+        private int VCLocked { get; set; }
+
+    }
+}
