07e257c69af73239ad6e76495b11eb749460ab8e
kl755999 <kl755999@LS302D24>
2025-04-24 19:01:16 -0500

    create tables in ApplicationDbContext

--- Data/ApplicationDbContext.cs
+++ Data/ApplicationDbContext.cs
@@ -1,13 +1,15 @@
-﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+﻿using CurrencyExchange.Models;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
 
 namespace CurrencyExchange.Data
 {
     public class ApplicationDbContext : IdentityDbContext
     {
-        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
-            : base(options)
-        {
-        }
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options) { }
+
+        public DbSet<User> users { get; set; } = null;
+        public DbSet<Order> orders { get; set; } = null;
+        public DbSet<Transaction> transactions { get; set; } = null;
     }
 }
