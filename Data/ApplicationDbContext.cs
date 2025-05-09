cea07923d59d05b23c56ec3fdc8a558e6204fb87
Leiracal <leiramontgomery@gmail.com>
2025-05-08 20:24:43 -0500

    Seeded Users, Wallets, and Orders with test data.

--- Data/ApplicationDbContext.cs
+++ Data/ApplicationDbContext.cs
@@ -1,6 +1,8 @@
 ﻿using CurrencyExchange.Models;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
+using System.Collections.Generic;
 
 namespace CurrencyExchange.Data
 {
@@ -22,12 +24,151 @@ namespace CurrencyExchange.Data
                 new OrderType { OrderTypeID = 1, Type = "Buy" },
                 new OrderType { OrderTypeID = 2, Type = "Sell" }
             );
+
             builder.Entity<OrderStatus>().HasData(
                 new OrderStatus { OrderStatusID = 1, Status = "Open" },
                 new OrderStatus { OrderStatusID = 2, Status = "Partial" },
                 new OrderStatus { OrderStatusID = 3, Status = "Filled" },
                 new OrderStatus { OrderStatusID = 4, Status = "Cancelled" }
             );
+
+            //Seed users and hash passwords
+            builder.Entity<IdentityUser>().HasData(
+                new IdentityUser
+                {
+                    Id = "953c886e-ac7b-45cd-9f70-30eaca6a5890",
+                    UserName = "aardvark@abbatoir.com",
+                    NormalizedUserName = "AARDVARK@ABBATOIR.COM",
+                    Email = "aardvark@abbatoir.com",
+                    NormalizedEmail = "AARDVARK@ABBATOIR.COM",
+                    EmailConfirmed = true,
+                    SecurityStamp = Guid.NewGuid().ToString("D"),
+                    //This is the hash for the password "P@ssword2534"
+                    PasswordHash = "AQAAAAEAACcQAAAAEM8DNhsKKQxKSgg7uwbHwLq89jZfLkeVfg+dWcEtUI6Cna+U8KLYMPb6c47ci5k5uA=="
+                },
+                new IdentityUser
+                {
+                    Id = "606f3c31-f721-4faf-9cd9-ed96c8b11f72",
+                    UserName = "aiko@aikowu.com",
+                    NormalizedUserName = "AIKO@AIKOWU.COM",
+                    Email = "aiko@aikowu.com",
+                    NormalizedEmail = "AIKO@AIKOWU.COM",
+                    EmailConfirmed = true,
+                    SecurityStamp = Guid.NewGuid().ToString("D"),
+                    //This is the hash for the password "P@ssword2534"
+                    PasswordHash = "AQAAAAEAACcQAAAAEM8DNhsKKQxKSgg7uwbHwLq89jZfLkeVfg+dWcEtUI6Cna+U8KLYMPb6c47ci5k5uA=="
+                },
+                new IdentityUser
+                {
+                    Id = "508f1f2c-21c8-42d9-806f-2cafc487bbc2",
+                    UserName = "kaneda@kuroda.com",
+                    NormalizedUserName = "KANEDA@KURODA.COM",
+                    Email = "kaneda@kuroda.com",
+                    NormalizedEmail = "KANEDA@KURODA.COM",
+                    EmailConfirmed = true,
+                    SecurityStamp = Guid.NewGuid().ToString("D"),
+                    //This is the hash for the password "P@ssword2534"
+                    PasswordHash = "AQAAAAEAACcQAAAAEM8DNhsKKQxKSgg7uwbHwLq89jZfLkeVfg+dWcEtUI6Cna+U8KLYMPb6c47ci5k5uA=="
+                },
+                new IdentityUser
+                {
+                    Id = "6d9715bc-eecb-4135-8e0e-8a9efd3139e3",
+                    UserName = "yuniq@epoch.com",
+                    NormalizedUserName = "YUNIQ@EPOCH.COM",
+                    Email = "yuniq@epoch.com",
+                    NormalizedEmail = "YUNIQ@EPOCH.COM",
+                    EmailConfirmed = true,
+                    SecurityStamp = Guid.NewGuid().ToString("D"),
+                    //This is the hash for the password "P@ssword2534"
+                    PasswordHash = "AQAAAAEAACcQAAAAEM8DNhsKKQxKSgg7uwbHwLq89jZfLkeVfg+dWcEtUI6Cna+U8KLYMPb6c47ci5k5uA=="
+                }
+                );
+
+            builder.Entity<Wallet>().HasData(
+                new Wallet
+                {
+                    WalletID = 1334,
+                    UserID = "953c886e-ac7b-45cd-9f70-30eaca6a5890",
+                    RMTBalance = 20000.0m,
+                    RMTLocked = 10000.0m,
+                    VCBalance = 1000,
+                    VCLocked = 0,
+                },
+                new Wallet
+                {
+                    WalletID = 1335,
+                    UserID = "606f3c31-f721-4faf-9cd9-ed96c8b11f72",
+                    RMTBalance = 20000.0m,
+                    RMTLocked = 5000.0m,
+                    VCBalance = 1000,
+                    VCLocked = 0,
+                },
+                new Wallet
+                {
+                    WalletID = 1336,
+                    UserID = "508f1f2c-21c8-42d9-806f-2cafc487bbc2",
+                    RMTBalance = 20000.0m,
+                    RMTLocked = 0.0m,
+                    VCBalance = 1000,
+                    VCLocked = 50,
+                },
+                new Wallet
+                {
+                    WalletID = 1337,
+                    UserID = "6d9715bc-eecb-4135-8e0e-8a9efd3139e3",
+                    RMTBalance = 20000.0m,
+                    RMTLocked = 0.0m,
+                    VCBalance = 1000,
+                    VCLocked = 50,
+                }
+                );
+
+            builder.Entity<Order>().HasData(
+                new Order
+                {
+                    OrderID = 1,
+                    UserID = "953c886e-ac7b-45cd-9f70-30eaca6a5890",
+                    OrderTypeID = 1,
+                    Price = 200m,
+                    Quantity = 50,
+                    Remaining = 50,
+                    OrderStatusID = 1,
+                    CreatedAt = new DateTime(2025, 5, 1, 14, 30, 0)
+                },
+                new Order
+                {
+                    OrderID = 2,
+                    UserID = "606f3c31-f721-4faf-9cd9-ed96c8b11f72",
+                    OrderTypeID = 1,
+                    Price = 100m,
+                    Quantity = 50,
+                    Remaining = 50,
+                    OrderStatusID = 1,
+                    CreatedAt = new DateTime(2025, 5, 2, 10, 15, 0)
+                },
+                new Order
+                {
+                    OrderID = 3,
+                    UserID = "953c886e-ac7b-45cd-9f70-30eaca6a5890",
+                    OrderTypeID = 2,
+                    Price = 300m,
+                    Quantity = 50,
+                    Remaining = 50,
+                    OrderStatusID = 1,
+                    CreatedAt = new DateTime(2025, 5, 1, 14, 30, 0)
+                },
+                new Order
+                {
+                    OrderID = 4,
+                    UserID = "953c886e-ac7b-45cd-9f70-30eaca6a5890",
+                    OrderTypeID = 2,
+                    Price = 400m,
+                    Quantity = 50,
+                    Remaining = 50,
+                    OrderStatusID = 1,
+                    CreatedAt = new DateTime(2025, 5, 1, 14, 30, 0)
+                }
+                );
         }
     }
 }
